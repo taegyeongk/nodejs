00100148b7fbff3d78fd727b52b04b15eabb17ed
taegyeongk~ <gofus2498@naver.com>
2023-11-30 17:51:43 +0900

    11.30 오후2

--- vue/component-project/src/views/DataBinding.vue
+++ vue/component-project/src/views/DataBinding.vue
@@ -4,6 +4,25 @@
         <h2 v-bind:name="'sample'" v-text="title" /> <!-- 태그 속성에 해당하는 고유 디렉티브 -->
         <p v-html="tagList"/> <!-- <strong> 태그는 보이지 않음 -->
         <p v-text="tagList"/> <!-- <strong> 태그 또한 문자열로 인식해서 나타냄 -->
+        <hr>
+        <input type="text" v-model="valueModel">
+        <p>{{ typeof valueModel }}</p>
+        <input type="number" v-model.lazy="numberModel">
+        <p>{{ numberModel + 10000 }}</p>
+        <select v-model="selectModel">
+            <option value="summer">여름</option>
+            <option value="winter">겨울</option>
+        </select>
+        <p>{{ selectModel }}</p>
+        <textarea v-model="textModel"/> <!-- 자기 자신이 많은 값을 가지고 있어서 p 는 필요없음 -->
+        <hr>
+        <input type="checkbox" v-model="chData" true-value="여" false-value="부">
+        <p>{{ chData }}</p>
+        <div>
+            <input type="checkbox" value="서울" v-model="city">서울
+            <input type="checkbox" value="대구" v-model="city">대구
+            <p>{{ city }}</p>
+        </div>
     </div>
 </template>
 
@@ -13,7 +32,13 @@
 
             return {
                 title : 'Hello, Vue.js',
-                tagList : '<strong>Today is ... </strong>'
+                tagList : '<strong>Today is ... </strong>',
+                valueModel : 'Korea',
+                numberModel : '0',
+                selectModel : 'winter',
+                textModel : '입력해보쇼',
+                chData : '',
+                city : ''
 
             }
         }
--- vue/vue-test/src/App.vue
+++ vue/vue-test/src/App.vue
@@ -2,7 +2,7 @@
 <VueComponent />
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/dataBinding">DataBinding</router-link>
   </nav>
   <router-view/>
 </template>
